b3ea2c6bb291434956dcda425ae60a4fa5b0c410
RENAN.BASTOS <renanbastos.tec@gmail.com>
2019-05-27 13:32:38 -0300

    Create const_var.go

---
+++ course/const_var.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	// m "math" -> For to create alias to package math 
+)
+
+func main() {
+	const PI = 3.1415
+	var raio = 3.2 // The compiler will identify automatically what type variable to declared by the value
+
+	// A variable declared should always be used
+	area := PI * math.Pow(raio, 2)
+
+	fmt.Println("This result is: ", area)
+
+	// Declare block variables
+	const (
+		a = 1
+		b = 2
+	)
+
+	var (
+		c = 3
+		d = 4
+	)
+
+	fmt.Println(a, b, c, d)
+}
